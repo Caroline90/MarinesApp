31a5572843977d433e757f6cfda0693871346a6f
Caroline90 <karolina2335@gmail.com>
2026-08-14 20:01:20 +0200

    Improve AIS no-data handling

--- src/main/java/app/maritime/AisStreamProperties.java
+++ src/main/java/app/maritime/AisStreamProperties.java
@@ -5,5 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "aisstream")
 record AisStreamProperties(boolean enabled, String url, String apiKey,
                            double minLongitude, double minLatitude,
-                           double maxLongitude, double maxLatitude) {
+                           double maxLongitude, double maxLatitude,
+                           boolean demoFallbackEnabled) {
 }
